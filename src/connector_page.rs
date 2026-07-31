f4dbc97206dabd3ba3da6cca5c579d72610ac2d2
Felipe Armoni <komyg@users.noreply.github.com>
2026-07-31 12:50:35 -0300

    Added first page

---
+++ src/connector_page.rs
@@ -0,0 +1,51 @@
+use iced::{
+    widget::{Column, column, text_input},
+};
+
+#[derive(Debug, Default)]
+pub struct ConnectorPage {
+    api_key: String,
+    api_secret: String,
+    api_url: String,
+    username: String,
+    room_id: String,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    APIKeyContentChanged(String),
+    APISecretContentChanged(String),
+    APIURLContentChanged(String),
+    UsernameContentChanged(String),
+    RoomIDContentChanged(String),
+}
+
+impl ConnectorPage {
+    pub fn view(&self) -> Column<'_, Message> {
+        column![
+            text_input("Api Key", &self.api_key)
+                .on_input(Message::APIKeyContentChanged),
+            text_input("Api Secret", &self.api_secret)
+                .on_input(Message::APISecretContentChanged)
+                .secure(true),
+            text_input("Api URL", &self.api_url)
+                .on_input(Message::APIURLContentChanged),
+            text_input("Username", &self.username)
+                .on_input(Message::UsernameContentChanged),
+            text_input("Room ID", &self.room_id)
+                .on_input(Message::RoomIDContentChanged),
+        ]
+        .spacing(10)
+        .padding(20)
+    }
+
+    pub fn update(&mut self, message: Message) {
+        match message {
+            Message::APIKeyContentChanged(value) => self.api_key = value,
+            Message::APISecretContentChanged(value) => self.api_secret = value,
+            Message::APIURLContentChanged(value) => self.api_url = value,
+            Message::UsernameContentChanged(value) => self.username = value,
+            Message::RoomIDContentChanged(value) => self.room_id = value,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod connector_page;
+
+use connector_page::ConnectorPage;
+
+pub fn main() -> iced::Result {
+    iced::application(
+        ConnectorPage::default,
+        ConnectorPage::update,
+        ConnectorPage::view,
+    )
+    .title("PV Meet Connector")
+    .run()
 }
