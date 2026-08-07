8271195f411309bc12d99f8e133aee5925abbb38
Felipe Armoni <komyg@users.noreply.github.com>
2026-08-06 22:30:48 -0300

    Added meeting room

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,64 @@ mod pages;
 
 use iced::{Element, Task};
 use pages::login::LoginPage;
+use pages::meeting_room::MeetingRoomPage;
 
 use crate::pages::login::{LoginPageAction, LoginPageMessage};
+use crate::pages::meeting_room::{MeetingRoomAction, MeetingRoomMessage};
+
+enum Screen {
+    Login(LoginPage),
+    MeetingRoom(MeetingRoomPage),
+}
+
+impl Screen {
+    fn new() -> Self {
+        Screen::Login(LoginPage::default())
+    }
+}
 
 #[derive(Debug, Clone)]
 enum Message {
     Login(LoginPageMessage),
+    MeetingRoom(MeetingRoomMessage),
 }
 
-fn update(page: &mut LoginPage, message: Message) -> Task<Message> {
+fn update(screen: &mut Screen, message: Message) -> Task<Message> {
     match message {
-        Message::Login(message) => match page.update(message) {
-            LoginPageAction::Login {
-                token: _token,
-                api_url: _api_url,
-            } => Task::none(),
-            LoginPageAction::None => Task::none(),
-        },
+        Message::Login(message) => {
+            let Screen::Login(page) = screen else {
+                return Task::none();
+            };
+
+            match page.update(message) {
+                LoginPageAction::Login { token, api_url } => {
+                    *screen = Screen::MeetingRoom(MeetingRoomPage::default(token, api_url));
+                    Task::none()
+                }
+                LoginPageAction::None => Task::none(),
+            }
+        }
+        Message::MeetingRoom(message) => {
+            let Screen::MeetingRoom(page) = screen else {
+                return Task::none();
+            };
+
+            match page.update(message) {
+                MeetingRoomAction::None => Task::none(),
+            }
+        }
     }
 }
 
-fn view(page: &LoginPage) -> Element<'_, Message> {
-    page.view().map(Message::Login)
+fn view(screen: &Screen) -> Element<'_, Message> {
+    match screen {
+        Screen::Login(page) => page.view().map(Message::Login),
+        Screen::MeetingRoom(page) => page.view().map(Message::MeetingRoom),
+    }
 }
 
 pub fn main() -> iced::Result {
-    iced::application(LoginPage::default, update, view)
+    iced::application(Screen::new, update, view)
         .title("PV Meet Connector")
         .run()
 }
--- src/pages/meeting_room.rs
+++ src/pages/meeting_room.rs
@@ -1,13 +1,51 @@
-use livekit::{Room, RoomOptions};
+use iced::{
+    Element,
+    widget::{column, text},
+};
+use livekit::{Room, RoomEvent, RoomOptions};
+use tokio::sync::mpsc::UnboundedReceiver;
 
-pub async fn connect_to_room(token: String, url: String) -> Result<(), String> {
+pub async fn connect_to_room(
+    token: String,
+    url: String,
+) -> Result<(Room, UnboundedReceiver<RoomEvent>), String> {
     let res = Room::connect(&url, &token, RoomOptions::default())
         .await
         .map_err(|e| e.to_string())?;
-    println!("Connected to room: {:#?}", res);
-    Ok(())
+    return Ok(res);
 }
 
-pub struct MeetingRoom {
-    room: Room,
+pub struct MeetingRoomPage {
+    room: Option<Room>,
+    events: Option<UnboundedReceiver<RoomEvent>>,
+    error: Option<String>,
+}
+
+#[derive(Debug, Clone)]
+pub enum MeetingRoomMessage {
+    None,
+}
+
+pub enum MeetingRoomAction {
+    None,
+}
+
+impl MeetingRoomPage {
+    pub fn default(token: String, url: String) -> Self {
+        Self {
+            room: None,
+            events: None,
+            error: None,
+        }
+    }
+
+    pub fn view(&self) -> Element<'_, MeetingRoomMessage> {
+        column![text("Meeting Room"),].into()
+    }
+
+    pub fn update(&mut self, message: MeetingRoomMessage) -> MeetingRoomAction {
+        match message {
+            MeetingRoomMessage::None => MeetingRoomAction::None,
+        }
+    }
 }
